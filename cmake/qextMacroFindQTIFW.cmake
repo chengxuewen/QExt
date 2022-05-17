ba4507448872145ee0af19d78720eb27a5db7287
DESKTOP-9RAJAM1\win <1398831004@qq.com>
2022-05-17 11:35:28 +0800

    V0.14.4.2，修复QuickControls2模块在Qt5.9版本cmake引入错误问题；修复QTIFW搜索脚本搜索不到Qt在线安装的IFW问题；

--- cmake/qextMacroFindQTIFW.cmake
+++ cmake/qextMacroFindQTIFW.cmake
@@ -25,6 +25,20 @@
 #################################################################################
 
 
+set(QEXT_QTIFW_SEARCH_SUB_PATHS "")
+set(_SEARCH_ROOT_PATHS "")
+macro(_findSubDirs)
+    set(root_path ${_SEARCH_ROOT_PATHS})
+    file(GLOB children RELATIVE ${root_path} ${root_path}/*)
+    list(APPEND QEXT_QTIFW_SEARCH_SUB_PATHS ${root_path})
+    foreach(child ${children})
+        if(IS_DIRECTORY ${root_path}/${child})
+            set(_SEARCH_ROOT_PATHS ${root_path}/${child})
+            _findSubDirs()
+        endif()
+    endforeach()
+endmacro(_findSubDirs)
+
 # Qt IFW Tools
 # ~~~~~~~~~~~~
 # QEXT_QTIFW_EXTRA_SEARCH_PATHS
@@ -52,19 +66,27 @@ macro(qextMacroFindQTIFW)
         file(GLOB children RELATIVE ${QEXT_QTIFW_SEARCH_PATHS} ${QEXT_QTIFW_SEARCH_PATHS}/*)
         foreach(child ${children})
             if(IS_DIRECTORY ${QEXT_QTIFW_SEARCH_PATHS}/${child})
-                if(${child} MATCHES "QtIFW" OR ${child} MATCHES "QtInstallerFramework")
-                    if(MSVC)
-                        find_program(QEXT_QTIFW_EXECUTABL NAMES binarycreator.exe PATHS ${QEXT_QTIFW_SEARCH_PATHS}/${child}/bin NO_DEFAULT_PATH)
-                    else()
-                        find_program(QEXT_QTIFW_EXECUTABL NAMES binarycreator PATHS ${QEXT_QTIFW_SEARCH_PATHS}/${child}/bin NO_DEFAULT_PATH)
-                    endif()
-                    if(NOT QEXT_QTIFW_EXECUTABL EQUAL "QEXT_QTIFW_EXECUTABL-NOTFOUND")
-                        set(QEXT_QTIFWDIR ${QEXT_QTIFW_SEARCH_PATHS}/${child})
-                        set(QEXT_QTIFWDIR_FOUND TRUE)
-                    endif()
+                if(${child} MATCHES "Tools")
+                    set(_SEARCH_ROOT_PATHS "${QEXT_QTIFW_SEARCH_PATHS}/${child}/QtInstallerFramework")
+                    _findSubDirs()
+                elseif(${child} MATCHES "QtIFW" OR ${child} MATCHES "QtInstallerFramework")
+                    set(_SEARCH_ROOT_PATHS "${QEXT_QTIFW_SEARCH_PATHS}/${child}")
+                    _findSubDirs()
                 endif()
             endif()
         endforeach()
+
+        # find exec
+        if(MSVC)
+            find_program(QEXT_QTIFW_EXECUTABL NAMES binarycreator.exe PATHS ${QEXT_QTIFW_SEARCH_SUB_PATHS} NO_DEFAULT_PATH)
+            find_path(QEXT_QTIFWDIR binarycreator.exe ${QEXT_QTIFW_SEARCH_SUB_PATHS})
+        else()
+            find_program(QEXT_QTIFW_EXECUTABL NAMES binarycreator PATHS ${QEXT_QTIFW_SEARCH_SUB_PATHS} NO_DEFAULT_PATH)
+            find_path(QEXT_QTIFWDIR binarycreator ${QEXT_QTIFW_SEARCH_SUB_PATHS})
+        endif()
+        if(NOT QEXT_QTIFW_EXECUTABL EQUAL "QEXT_QTIFW_EXECUTABL-NOTFOUND")
+            set(QEXT_QTIFWDIR_FOUND TRUE)
+        endif()
 
         if(NOT ${QEXT_QTIFWDIR_FOUND})
             if(QEXT_QTIFWDIR_FIND_REQUIRED)
