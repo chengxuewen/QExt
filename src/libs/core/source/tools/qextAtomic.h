f43179bf99370cc6cc6c031f7d5f1b156353050d
chengxuewen <cxw@huanweitech.com>
2024-05-17 11:44:01 +0800

    Add QExtAtomic unit test.

--- src/libs/core/source/tools/qextAtomic.h
+++ src/libs/core/source/tools/qextAtomic.h
@@ -3,10 +3,39 @@
 
 #include <qextGlobal.h>
 
-class QExtAtomic
+#include <QAtomicPointer>
+
+class QExtAtomicDouble
 {
 public:
-    QExtAtomic();
+    QExtAtomicDouble() : m_atomic(0) { }
+    QExtAtomicDouble(double value) : m_atomic(doubleToInt64Ptr(value)) {}
+    virtual ~QExtAtomicDouble() {}
+
+    static double doubleFromInt64Ptr(qint64 *intVal)
+    {
+        return doubleFromInt64(breinterpret_cast<qint64>(intVal));
+    }
+    static double doubleFromInt64(qint64 intVal)
+    {
+        double dblValue = 0;
+        memcpy(&dblValue, &intVal, qMin(sizeof(dblValue), sizeof(intVal)));
+        return dblValue;
+    }
+
+    static qint64 *doubleToInt64Ptr(double dblValue)
+    {
+        return reinterpret_cast<qint64 *>(doubleToInt64(dblValue));
+    }
+    static qint64 doubleToInt64(double dblValue)
+    {
+        qint64 intVal = 0;
+        memcpy(&intVal, &dblValue, qMin(sizeof(intVal), sizeof(dblValue)));
+        return intVal;
+    }
+
+private:
+    QAtomicPointer<qint64> m_atomic;
 };
 
 #endif // _QEXTATOMIC_H
---
+++ src/libs/core/tests/tools/tst_qextAtomic.cpp
@@ -0,0 +1,25 @@
+﻿#include <qextAtomic.h>
+
+#include <QtTest>
+#include <QtEndian>
+#include <QByteArray>
+#include <QDebug>
+
+class QExtAtomicTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void doubleTest();
+};
+
+
+
+
+void QExtAtomicTest::doubleTest()
+{
+
+}
+
+QTEST_APPLESS_MAIN(QExtAtomicTest)
+
+#include <tst_qextAtomic.moc>
