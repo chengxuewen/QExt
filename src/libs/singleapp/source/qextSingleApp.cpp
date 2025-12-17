62f9a89cb62d854002a3581c5ae39857bb510e4a
DESKTOP-OJ42NFK\win <1398831004@qq.com>
2025-12-17 10:51:46 +0800

    Add QExtSingleApp class to encapsulate the SingleApplication library API.

--- src/libs/singleapp/source/qextSingleApp.cpp
+++ src/libs/singleapp/source/qextSingleApp.cpp
@@ -1,1 +1,44 @@
 ﻿#include <qextSingleApp.h>
+
+#include <singleapplication.h>
+
+class QExtSingleAppPrivate
+{
+    QExtSingleApp * const q_ptr;
+    QEXT_DECLARE_PUBLIC(QExtSingleApp)
+    QEXT_DECLARE_DISABLE_MOVE(QExtSingleAppPrivate)
+public:
+    explicit QExtSingleAppPrivate(QExtSingleApp *p);
+    virtual ~QExtSingleAppPrivate();
+
+    QScopedPointer<SingleApplication> mSingleApplication;
+};
+
+QExtSingleAppPrivate::QExtSingleAppPrivate(QExtSingleApp *p)
+    : q_ptr(p)
+{
+}
+
+QExtSingleAppPrivate::~QExtSingleAppPrivate()
+{
+}
+
+QExtSingleApp::QExtSingleApp(int &argc, char *argv[])
+    : dd_ptr(new QExtSingleAppPrivate(this))
+{
+    Q_D(QExtSingleApp);
+    d->mSingleApplication.reset(new SingleApplication(argc, argv));
+    connect(d->mSingleApplication.data(), &SingleApplication::instanceStarted, this, &QExtSingleApp::instanceStarted);
+    connect(d->mSingleApplication.data(), &SingleApplication::receivedMessage, this, &QExtSingleApp::receivedMessage);
+}
+
+QExtSingleApp::~QExtSingleApp()
+{
+}
+
+bool QExtSingleApp::sendMessage(const QByteArray &message)
+{
+    Q_D(QExtSingleApp);
+    return d->mSingleApplication->sendMessage(message);
+}
+
--- src/libs/singleapp/source/qextSingleApp.h
+++ src/libs/singleapp/source/qextSingleApp.h
@@ -3,8 +3,40 @@
 
 #include <qextSingleAppGlobal.h>
 
-#include <singleapplication.h>
+#include <QObject>
 
-typedef SingleApplication QExtSingleApp;
+class QExtSingleAppPrivate;
+class QEXT_SINGLEAPP_API QExtSingleApp : public QObject
+{
+    Q_OBJECT
+public:
+    QExtSingleApp(int &argc, char *argv[]);
+    ~QExtSingleApp() QEXT_OVERRIDE;
+
+    /**
+     * @brief Sends a message to the primary instance
+     * @param message data to send
+     * @returns `true` on success
+     * @note sendMessage() will return false if invoked from the primary instance
+     */
+    bool sendMessage(const QByteArray &message);
+
+Q_SIGNALS:
+    /**
+     * @brief Triggered whenever a new instance had been started,
+     * except for secondary instances if the `Mode::SecondaryNotification` flag is not specified
+     */
+    void instanceStarted();
+
+    /**
+     * @brief Triggered whenever there is a message received from a secondary instance
+     */
+    void receivedMessage(quint32 instanceId, QByteArray message);
+
+protected:
+    QScopedPointer<QExtSingleAppPrivate> dd_ptr;
+    QEXT_DECLARE_PRIVATE_D(dd_ptr, QExtSingleApp)
+    QEXT_DECLARE_DISABLE_COPY_MOVE(QExtSingleApp)
+};
 
 #endif //_QEXTSINGLEAPP_H
